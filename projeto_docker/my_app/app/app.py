6c10328c34136d0aed485c651561d41ef4e7794a
allanescher <allanbescher@hotmail.com>
2025-02-27 23:40:08 -0300

    main

--- projeto_docker/my_app/app/app.py
+++ projeto_docker/my_app/app/app.py
@@ -15,3 +15,8 @@ def create_app():
         db.create_all()
 
     return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- projeto_docker/my_app/app/routes.py
+++ projeto_docker/my_app/app/routes.py
@@ -1,8 +1,6 @@
 from flask import request, jsonify
-from . import create_app, db
+from . import app, db
 from .models import Item
-
-app = create_app()
 
 @app.route('/items', methods=['POST'])
 def add_item():
@@ -17,6 +15,3 @@ def get_items():
     items = Item.query.all()
     result = [{'id': item.id, 'name': item.name} for item in items]
     return jsonify(result), 200
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
